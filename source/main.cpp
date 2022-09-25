bf8ad3d0d9c8852d4122ad44ccfd8fc5c1bfc565
ioannoid <io.a.skoul@gmail.com>
2022-09-25 15:58:59 -0400

    Started rewrite of dashboard in C++ using sdl2

---
+++ source/main.cpp
@@ -0,0 +1,50 @@
+#include <SDL2/SDL_render.h>
+#include <SDL2/SDL_video.h>
+#include <iostream>
+
+#include <SDL2/SDL.h>
+
+int main() {
+
+    SDL_Window* window = nullptr;
+    SDL_Renderer* renderer = nullptr;
+
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        std::cerr << "SDL failed to initizalize: " << SDL_GetError() << "\n";
+        return -1;
+    }
+
+    window =
+        SDL_CreateWindow("Dashboard", SDL_WINDOWPOS_UNDEFINED,
+                         SDL_WINDOWPOS_UNDEFINED, 1280, 720, SDL_WINDOW_SHOWN);
+    if (!window) {
+        std::cerr << "SDL window could not be created: " << SDL_GetError()
+                  << "\n";
+        return -1;
+    }
+
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (!renderer) {
+        std::cerr << "SDL renderer could not be creat: " << SDL_GetError()
+                  << "\n";
+    }
+
+    SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF);
+    SDL_RenderClear(renderer);
+    SDL_RenderPresent(renderer);
+
+    SDL_Event e;
+    bool quit = false;
+    while (!quit) {
+        while (SDL_PollEvent(&e)) {
+            if (e.type == SDL_QUIT)
+                quit = true;
+        }
+    }
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return 0;
+}
