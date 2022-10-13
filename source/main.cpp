3d9e78b60d6606c26ccf3fca3ff14daebaa4370b
ioannoid <io.a.skoul@gmail.com>
2022-10-13 17:28:10 -0400

    Started working on speedometer and tachyometer

--- source/main.cpp
+++ source/main.cpp
@@ -8,6 +8,41 @@
 
 using namespace cv;
 
+void DrawCircle(SDL_Renderer* renderer, int32_t centerX, int32_t centerY,
+                int32_t radius) {
+    const int32_t diameter = (radius * 2);
+
+    int32_t x = (radius - 1);
+    int32_t y = 0;
+    int32_t tx = 1;
+    int32_t ty = 1;
+    int32_t error = (tx - diameter);
+
+    while (x >= y) {
+        //  Each of the following renders an octant of the circle
+        SDL_RenderDrawPoint(renderer, centerX + x, centerY - y);
+        SDL_RenderDrawPoint(renderer, centerX + x, centerY + y);
+        SDL_RenderDrawPoint(renderer, centerX - x, centerY - y);
+        SDL_RenderDrawPoint(renderer, centerX - x, centerY + y);
+        SDL_RenderDrawPoint(renderer, centerX + y, centerY - x);
+        SDL_RenderDrawPoint(renderer, centerX + y, centerY + x);
+        SDL_RenderDrawPoint(renderer, centerX - y, centerY - x);
+        SDL_RenderDrawPoint(renderer, centerX - y, centerY + x);
+
+        if (error <= 0) {
+            ++y;
+            error += ty;
+            ty += 2;
+        }
+
+        if (error > 0) {
+            --x;
+            tx += 2;
+            error += (tx - diameter);
+        }
+    }
+}
+
 int main() {
 
     SDL_Window* window = nullptr;
@@ -33,12 +68,27 @@ int main() {
                   << "\n";
     }
 
+    dash_renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+    int width, height;
+    SDL_GetWindowSize(window, &width, &height);
+
     VideoCapture capture("/dev/video0");
+    capture.set(CAP_PROP_FRAME_WIDTH, 400);
+    capture.set(CAP_PROP_FRAME_HEIGHT, 300);
     Mat frame;
     capture >> frame;
     SDL_Texture* tex =
         SDL_CreateTexture(renderer, SDL_PIXELFORMAT_BGR24,
                           SDL_TEXTUREACCESS_STREAMING, frame.cols, frame.rows);
+
+    int twidth, theight;
+    SDL_QueryTexture(tex, nullptr, nullptr, &twidth, &theight);
+    std::cout << width << ", " << height << "\n";
+    std::cout << twidth << ", " << theight << "\n";
+
+    SDL_Rect camera_rect = {width / 2 - twidth / 2, height / 2 - theight / 2,
+                            twidth, theight};
 
     SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF);
     SDL_RenderClear(renderer);
@@ -50,14 +100,16 @@ int main() {
         while (SDL_PollEvent(&e)) {
             if (e.type == SDL_QUIT)
                 quit = true;
-
-            capture >> frame;
-            SDL_UpdateTexture(tex, NULL, (void*)frame.data, frame.step1());
-
-            SDL_RenderClear(renderer);
-            SDL_RenderCopy(renderer, tex, NULL, NULL);
-            SDL_RenderPresent(renderer);
         }
+
+        capture >> frame;
+        SDL_UpdateTexture(tex, NULL, (void*)frame.data, frame.step1());
+
+        SDL_RenderClear(renderer);
+
+        SDL_RenderCopy(renderer, tex, NULL, &camera_rect);
+
+        SDL_RenderPresent(renderer);
     }
 
     SDL_DestroyTexture(tex);
